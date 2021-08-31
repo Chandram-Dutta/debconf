b6cb47381f0216d2d39aa362e6a2fbba1443b790
Chandram-Dutta <chandramdutta2004@gmail.com>
2021-08-31 16:19:08 +0530

    Minor Changes

--- lib/screens/home/HomePage.dart
+++ lib/screens/home/HomePage.dart
@@ -1,10 +1,9 @@
+import 'package:DebConf/navBar/BottomNavBar.dart';
 import 'package:DebConf/screens/home/widgets/HomePageMainColumn.dart';
-import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,52 +17,3 @@ class HomePage extends StatelessWidget {
     );
   }
 }
-
-class BotttomNavBar extends StatefulWidget {
-  const BotttomNavBar({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  _BotttomNavBarState createState() => _BotttomNavBarState();
-}
-
-class _BotttomNavBarState extends State<BotttomNavBar> {
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBottomNavigationBar(
-      icons: [
-        Icons.home,
-        Icons.date_range,
-      ],
-      activeIndex: _selectedIndex,
-      onTap: _onItemTapped,
-      backgroundColor: Theme.of(context).primaryColor,
-      splashColor: Theme.of(context).accentColor,
-      activeColor: Theme.of(context).accentColor,
-      notchSmoothness: NotchSmoothness.verySmoothEdge,
-    );
-  }
-}
-
-
-// items: [
-//         BottomNavigationBarItem(
-//           icon: new Icon(Icons.home),
-//           label: "Home",
-//         ),
-//         BottomNavigationBarItem(
-//           icon: new Icon(Icons.date_range),
-//           label: "Schedule",
-//         ),
-//       ],
-//       currentIndex: _selectedIndex,
-//       onTap: _onItemTapped,
