fa54845432a470af2da9f4c4d8cf0e2013637699
Chandram-Dutta <chandramdutta2004@gmail.com>
2021-09-17 11:42:49 +0530

    Changes in Schedule

--- lib/lists/schedule/schedule.dart
+++ lib/lists/schedule/schedule.dart
@@ -4,40 +4,45 @@ import 'package:syncfusion_flutter_calendar/calendar.dart';
 List<Appointment> getAppointment(BuildContext context) {
   List<Appointment> meeting = <Appointment>[
     Appointment(
-        startTime: DateTime(2021, 8, 2, 9, 0, 0),
-        endTime: DateTime(2021, 8, 2, 10, 0, 0),
-        subject: "Lorem Ipsum",
-        notes: "https://www.google.com",
+        startTime: DateTime(2021, 8, 24, 12, 45, 0),
+        endTime: DateTime(2021, 8, 24, 13, 0, 0),
+        subject: "DebConf21 Opening",
+        notes: "https://debconf21.debconf.org/talks/108-debconf21-opening/",
         color: Theme.of(context).colorScheme.secondary,
-        location: "Lorem Ipsum"),
+        location: "Online"),
     Appointment(
-        startTime: DateTime(2021, 8, 2, 10, 0, 0),
-        endTime: DateTime(2021, 8, 2, 11, 0, 0),
-        subject: "dolor sit",
-        notes: "https://www.google.com",
+        startTime: DateTime(2021, 8, 24, 13, 0, 0),
+        endTime: DateTime(2021, 8, 24, 13, 45, 0),
+        subject:
+            "OpenStack Cluster Installer in Bullseye: what's new, what to expect",
+        notes:
+            "https://debconf21.debconf.org/talks/26-openstack-cluster-installer-in-bullseye-whats-new-what-to-expect/",
         color: Theme.of(context).colorScheme.secondary,
-        location: "dolor sit"),
+        location: "Online"),
     Appointment(
-        startTime: DateTime(2021, 8, 2, 11, 0, 0),
-        endTime: DateTime(2021, 8, 2, 12, 0, 0),
-        subject: "amet, consectetur",
-        notes: "https://www.google.com",
+        startTime: DateTime(2021, 8, 24, 14, 00, 0),
+        endTime: DateTime(2021, 8, 24, 14, 45, 0),
+        subject:
+            "branch2repo -- enabling casual contributions to debian-installer",
+        notes:
+            "https://debconf21.debconf.org/talks/30-branch2repo-enabling-casual-contributions-to-debian-installer/",
         color: Theme.of(context).colorScheme.secondary,
-        location: "amet, consectetur"),
+        location: "Online"),
     Appointment(
-        startTime: DateTime(2021, 8, 2, 12, 0, 0),
-        endTime: DateTime(2021, 8, 2, 13, 0, 0),
-        subject: "adipiscing elit,",
-        notes: "https://www.google.com",
+        startTime: DateTime(2021, 8, 24, 14, 00, 0),
+        endTime: DateTime(2021, 8, 24, 14, 45, 0),
+        subject:
+            "బాల స్వేచ్ఛ ప్రాజెక్టు - కంప్యూటర్ ఆధారిత విద్యలో ఒక కొత్త ఒరవడి",
+        notes: "https://debconf21.debconf.org/talks/61--/",
         color: Theme.of(context).colorScheme.secondary,
-        location: "adipiscing elit,"),
+        location: "Online"),
     Appointment(
-        startTime: DateTime(2021, 8, 2, 13, 0, 0),
-        endTime: DateTime(2021, 8, 2, 14, 0, 0),
-        subject: "sed do eiusmod",
-        notes: "https://www.google.com",
+        startTime: DateTime(2021, 8, 24, 15, 00, 0),
+        endTime: DateTime(2021, 8, 24, 15, 20, 0),
+        subject: "	వాడుక లో డెబియన్ మెడ్! (Debian Med in Use)",
+        notes: "https://debconf21.debconf.org/talks/108-debconf21-opening/",
         color: Theme.of(context).colorScheme.secondary,
-        location: "sed do eiusmod"),
+        location: "Online"),
   ];
 
   return meeting;
